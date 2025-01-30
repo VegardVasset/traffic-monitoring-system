c8136040bfb0107664230b94e49aea4e4def15a6
Jonatan Valen <102178908+jvalen01@users.noreply.github.com>
2025-01-30 11:11:41 +0100

    Installed Shadcn and added some UI components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import { fetchTrafficData } from "./lib/api";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
     const [data, setData] = useState([]);
@@ -14,15 +16,18 @@ export default function Home() {
     }, []);
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Traffic Monitoring Dashboard</h1>
-            <ul>
-                {data.map((item, index) => (
-                    <li key={index} className="border p-2 mt-2">
-                        {JSON.stringify(item)}
-                    </li>
-                ))}
-            </ul>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Traffic Monitoring Dashboard</h1>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index} className="border p-2 mt-2">
+              {JSON.stringify(item)}
+            </li>
+          ))}
+        </ul>
+        <div>
+          <Button>Click me</Button>
         </div>
+      </div>
     );
 }
