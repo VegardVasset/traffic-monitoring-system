4043ceb1b408b64386b3f835abe437741f6503fd
vegardvasset <vva034@student.uib.no>
2025-03-17 13:48:22 +0100

    added speed anomaly detector

--- frontend/src/app/dts/events/page.tsx
+++ frontend/src/app/dts/events/page.tsx
@@ -3,11 +3,14 @@
 import React from "react";
 import EventTemplate from "@/components/shared/pageTemplates/eventTemplate";
 import { DataProvider } from "@/context/DataContext";
+import SpeedAnomalyAlert from "@/components/alerts/speedAnomalyAlert";
 
-export default function TiresEventsPage() {
+export default function DTSEventsPage() {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/dts`;
   return (
     <DataProvider apiUrl={apiUrl} domain="dts">
+      {/* Display speed anomaly alert for DTS events */}
+      <SpeedAnomalyAlert />
       <EventTemplate domain="dts" />
     </DataProvider>
   );
--- frontend/src/app/tires/events/page.tsx
+++ frontend/src/app/tires/events/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import EventTemplate from "@/components/shared/pageTemplates/eventTemplate";
 import { DataProvider } from "@/context/DataContext";
-import AnomalyAlert from "@/components/alerts/anomalyAlert";
+import AnomalyAlert from "@/components/alerts/tireAnomalyAlert";
 
 export default function TiresEventsPage() {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tires`;
